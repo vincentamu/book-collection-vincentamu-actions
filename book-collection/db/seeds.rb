c05e05e6d8db12f59ba1c6d5bbdeb87025b360e2
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-09-06 13:46:04 -0500

    qhe

--- book-collection/db/seeds.rb
+++ book-collection/db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create!([
+    {title:a},
+    {title:b},
+    {title:c},
+    {title:d},
+    {title:e}
+])
